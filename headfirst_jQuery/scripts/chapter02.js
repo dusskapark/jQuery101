6aa5c2f4720aac5470c65fe2c4f7d20b4fa7de91
dusskapark <dusskapark@gmail.com>
2014-05-25 17:32:42 +0900

    bug fix

--- headfirst_jQuery/scripts/chapter02.js
+++ headfirst_jQuery/scripts/chapter02.js
@@ -1,13 +1,4 @@
-/** 
-@(document).ready(fucntion() {
-	$("guess_box").click(fucntion(){
-		alert("you click me");
-	});
-});
- * 
- */
-
-@(document).ready(fuction() {
+$(document).ready(fuction() {
 	$("guess_box").click(fuction() {
 		$("guessbox").remove();
 		var = discount=Math.floor((Math.radom()*5)+5);
