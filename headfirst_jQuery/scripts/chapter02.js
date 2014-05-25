4814eb77d117c129bf934ab3b09f76c65a7998ee
dusskapark <dusskapark@gmail.com>
2014-05-25 20:26:23 +0900

    #2 bug fixed

--- headfirst_jQuery/scripts/chapter02.js
+++ headfirst_jQuery/scripts/chapter02.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	$(".guess_box").click(function() {
-		$(".guess_box").remove();
+		$(".guess_box p").remove();
 		var discount = Math.floor((Math.random()*5)+5);
 		var discount_msg = "<p>your discount is "+ discount +"% </p>";
 		alert(discount_msg);
