dff586f86aa5cb6adb0eeaa74917e7d0054ca7c9
dusskapark <dusskapark@gmail.com>
2014-05-25 17:38:34 +0900

    ¿ÀŸ¼öÁ¤

--- headfirst_jQuery/scripts/chapter02.js
+++ headfirst_jQuery/scripts/chapter02.js
@@ -1,8 +1,8 @@
-$(document).ready(fuction() {
-	$("guess_box").click(fuction() {
-		$("guessbox").remove();
-		var = discount=Math.floor((Math.radom()*5)+5);
-		var = discount_msg="<p>your discount is "+ discount +%</p>;
+$(document).ready(function() {
+	$("guess_box").click(function() {
+		$("guess_box").remove();
+		var discount = Math.floor((Math.random()*5)+5);
+		var discount_msg = "<p>your discount is "+ discount +"% </p>";
 		alert(discount_msg);
 		$(this).append(discount_msg);
 	});
