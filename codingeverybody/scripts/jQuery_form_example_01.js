6f4302a37bcade6b60a56b7777b7d83cd32c95eb
dusskapark <dusskapark@gmail.com>
2014-05-27 01:15:16 +0900

    add coding eveybody examples

---
+++ codingeverybody/scripts/jQuery_form_example_01.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+            $("input").focus( function () {
+                $(this).next("span").html('focus');
+            }).blur( function() {
+                $(this).next("span").html('blur');
+            }).change(function(e){
+                alert('change!! '+$(e.target).val());
+            }).select(function(){
+                $(this).next('span').html('select');
+            });
