3e104c994e4a37d20f20305daedfd0d4bb0c0783
dusskapark <dusskapark@gmail.com>
2014-05-28 11:43:50 +0900

    add form examples

--- codingeverybody/scripts/jQuery_form_example_01.js
+++ codingeverybody/scripts/jQuery_form_example_01.js
@@ -1,12 +1,11 @@
-/**
- * 
- */
-            $("input").focus( function () {
-                $(this).next("span").html('focus');
-            }).blur( function() {
-                $(this).next("span").html('blur');
-            }).change(function(e){
-                alert('change!! '+$(e.target).val());
-            }).select(function(){
-                $(this).next('span').html('select');
-            });
+$(document).ready(function() {
+	$(input).focus( function() {
+		$(this).next("span").html('focus');
+		}).blur( function() {
+			$(this).next("span").html('blur');
+			}).change(function(e) {
+				alert('change!! '+$(e.target).val());
+				}).select(function(){
+					$(this).next('span').html('select');
+					});
+});
