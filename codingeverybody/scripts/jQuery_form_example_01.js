b18feb5934d1c4952556ca1623644f3f8ba888a0
dusskapark <dusskapark@gmail.com>
2014-05-29 14:20:34 +0900

    Merge Branches

--- codingeverybody/scripts/jQuery_form_example_01.js
+++ codingeverybody/scripts/jQuery_form_example_01.js
@@ -1,11 +1,9 @@
-$(document).ready(function() {
-	$(input).focus( function() {
-		$(this).next("span").html('focus');
-		}).blur( function() {
-			$(this).next("span").html('blur');
-			}).change(function(e) {
-				alert('change!! '+$(e.target).val());
-				}).select(function(){
-					$(this).next('span').html('select');
-					});
+$("input").focus( function () {
+	$(this).next("span").html('focus');
+	}).blur( function() {
+		$(this).next("span").html('blur');
+		}).change(function(e){
+			alert('change!! '+$(e.target).val());
+			}).select(function(){
+				$(this).next('span').html('select');
 });
