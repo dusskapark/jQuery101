d746a871c69a7c3e8c90cac1fe675f9674a22ec5
dusskapark <dusskapark@gmail.com>
2014-05-30 00:15:07 +0900

    #4 chapter 3: each() and add git_ignore_list about eclipse

--- headfirst_jQuery/chapter03/scripts/my_scripts.js
+++ headfirst_jQuery/chapter03/scripts/my_scripts.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
 	$(".guess_box").click(function() {
-		$(".guess_box p").remove();
 		var discount = Math.floor((Math.random()*5)+5);
 		var discount_msg = "<p>your discount is "+ discount +"% </p>";
-		alert(discount_msg);
 		$(this).append(discount_msg);
-		$(this).unbind("click");
+		$(".guess_box").each( function(){
+			$(this).unbind("click");
+		});
+		
 	});
 });
