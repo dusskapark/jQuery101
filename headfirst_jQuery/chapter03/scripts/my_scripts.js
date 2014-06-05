801c8ee9ea2754007c195f72d7ebf1d2613c4db8
dusskapark <dusskapark@gmail.com>
2014-06-05 23:51:38 +0900

    #12 choose <div> by click

--- headfirst_jQuery/chapter03/scripts/my_scripts.js
+++ headfirst_jQuery/chapter03/scripts/my_scripts.js
@@ -13,36 +13,27 @@ $(document).ready(function() {
 		var my_num = Math.floor(Math.random()*num);
 		return my_num;
 	}
-		var hideCode = function () {
-		var numRoad = getRandom(4);
-		$(".guess_box").each(function(index, value) {
-			if (numRoad == index) {
-				$(this).append("<span id='has_discount'>&nbsp;</span>");
-				return false;
-			}
+	function checkForCode () {
+		var discount;
+		$(this).append("<span id='has_discount'>&nbsp;</span>");
+		
+		if($.contains(this, document.getElementById("has_discount") ) ) 
+		{
+			var my_num = getRandom(100);
+			//p158에 따라서 내용 수
+			discount = "<p>Your Code: Code"+my_num+"% </p>"; 
+		 } else {
+			 discount = "<p>Sorry, no discount this time!</p>";
+		 }
+	 $(".guess_box").each( function() {
+		 if($.contains(this, document.getElementById("has_discount")) ) {
+			 $(this).addClass("discount");
+		 }else {
+			 $(this).addClass("no_discount");
+		 }
+		 $(this).unbind();
 		});
-	};
-		hideCode();
-		function checkForCode () {
-			var discount;
-			if($.contains(this, document.getElementById("has_discount") ) ) 
-			{
-				var my_num = getRandom(100);
-				//p158에 따라서 내용 수
-				discount = "<p>Your Code: Code"+my_num+"% </p>"; 
-			 } else {
-				 discount = "<p>Sorry, no discount this time!</p>";
-			 }
-		 $(".guess_box").each( function() {
-			 if($.contains(this, document.getElementById("has_discount")) ) {
-				 $(this).addClass("discount");
-			 }else {
-				 $(this).addClass("no_discount");
-			 }
-			 $(this).unbind();
-			});
-		 $("#result").append(discount);
-	}
+	 $("#result").append(discount);
+}
 });
-
 
