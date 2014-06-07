5cb4551add90b86faa77a92d25f880df6077004b
dusskapark <dusskapark@gmail.com>
2014-06-08 00:46:38 +0900

    #13 #16 관련 코드 추가
    관련 코드 추

--- headfirst_jQuery/chapter04/begin/scripts/my_scripts.js
+++ headfirst_jQuery/chapter04/begin/scripts/my_scripts.js
@@ -1,5 +1,13 @@
-
-$(document).ready(function(){
-
-	
+$(document).ready(function() {
+	var v = false;
+	$("button#vegOn").click(function(){
+		if (v==false) {
+			v = true;
+		}
+	});
+	$("button#restoreMe").click(function(){
+		if (v==true) {
+			v = false;
+		}
+	});
 });
