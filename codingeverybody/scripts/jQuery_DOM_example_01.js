b2d5a424aa9f5413df717e43a5e35f576dab2fef
dusskapark <dusskapark@gmail.com>
2014-06-19 14:35:55 +0900

    플라랩 예제소스 커밋
    DOM 을 가지고 놀아보는 소스를 커밋 합니다.

---
+++ codingeverybody/scripts/jQuery_DOM_example_01.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+var $wrapper = $('#root').addClass('selected');
+$('#code').keydown(function(e) {
+	if (e.keyCode == 13) {
+		eval($(this).val());
+		return false;
+	}
+}).change(function() {
+	eval($(this).val());
+});
+$('tr').click(function() {
+	$(this).find('.title').toggleClass('checked');
+})
--- headfirst_jQuery/chapter04/begin/scripts/my_scripts.js
+++ headfirst_jQuery/chapter04/begin/scripts/my_scripts.js
@@ -10,4 +10,7 @@ $(document).ready(function() {
 			v = false;
 		}
 	});
+	$("jungseob.good.hansome.apple").click(function() {
+		console.log("멋진 사람을 알아보셨습니다! 외장하드 고마워요~ 비타500 놓고감!");
+	});
 });
