22d3e58a11c3a9ba9d62c1340b12f714a43a8372
dusskapark <dusskapark@gmail.com>
2015-09-02 13:43:08 +0900

    GA코드 개선
    GA코드를 추가 및 개선했습니다.

--- parallax/js/init.js
+++ parallax/js/init.js
@@ -3,26 +3,37 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
+
+    // Menu의 버튼을 눌렀는지 분석
     $(".gnb > li > a").click(function(){
       var content = $(this).text();
       console.log(content);
       ga('send', 'event', 'category', 'clicked', content );
     });
 
+    // 상세 내용을 읽었는지 / 어떤 것을 읽었는지 분석
     $('.collapsible-header').click(function(){
       var header = $(this).text();
       console.log(header);
       ga('send', 'event', 'description', 'clicked', header);
     })
 
-    $('#download-button')
+    // 신청하기 버튼을 어떤 것을 눌렀는지 분석
+    $('.btn-large').click(function(){
+      var id = $(this).parents('div[id]').attr('id');
+      console.log(id);
+      ga('send', 'event', 'enrollment', 'clicked', id);
+    });
 
-    // var options = [
-    //   {selector: '.classFri', offset: 200, callback: 'Materialize.toast("기초/이론반", 1500, 'rounded' )' },
-    //   {selector: '.classTue', offset: 200, callback: 'Materialize.toast("실습반", 1500, 'rounded' )' },
-    // ];
-    // Materialize.scrollFire(options);
-
+    // //스크롤 시 GA코드 보내기
+    // function() {
+    //   var options = [
+    //     {selector: '.classFri', offset: 200, callback: 'Materialize.toast("기초/이론반", 1500, 'rounded' )' },
+    //     {selector: '.classTue', offset: 200, callback: 'Materialize.toast("실습반", 1500, 'rounded' )' },
+    //   ];
+    //   Materialize.scrollFire(options);
+    //
+    // }
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
