f8babe308caacf430bb48091a70e974b5f95d382
dusskapark <dusskapark@gmail.com>
2015-09-02 11:50:29 +0900

    GA추가 및 수정
    GA추가 및 수

--- parallax/js/init.js
+++ parallax/js/init.js
@@ -3,6 +3,26 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
+    $(".gnb > li > a").click(function(){
+      var content = $(this).text();
+      console.log(content);
+      ga('send', 'event', 'category', 'clicked', content );
+    });
+
+    $('.collapsible-header').click(function(){
+      var header = $(this).text();
+      console.log(header);
+      ga('send', 'event', 'description', 'clicked', header);
+    })
+
+    $('#download-button')
+
+    // var options = [
+    //   {selector: '.classFri', offset: 200, callback: 'Materialize.toast("기초/이론반", 1500, 'rounded' )' },
+    //   {selector: '.classTue', offset: 200, callback: 'Materialize.toast("실습반", 1500, 'rounded' )' },
+    // ];
+    // Materialize.scrollFire(options);
+
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
