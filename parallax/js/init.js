fe8c034573a150ad9bdaa31ac043008becd9c739
dusskapark <dusskapark@gmail.com>
2015-10-02 00:34:11 +0900

    카카오버튼 수

--- parallax/js/init.js
+++ parallax/js/init.js
@@ -13,15 +13,15 @@
             Kakao.Link.sendTalkLink({
               label: "[원스토어] 인앱SDK 및 검증 정책 업데이트 안내! 인앱SDK로 Android 6.0에 완벽 대응하고 업데이트 검증으로 빠르게 출시하세요!",
               image: {
-                src: "http://dusskapark.github.io/jQuery101/parallax/img/kakao-touch-icon-150.png",
-                width: "150px",
-                height: "150px"
+                src: "http://dusskapark.github.io/jQuery101/parallax/img/oct-touch-icon-150.png",
+                // width: "150px",
+                // height: "150px"
               },
               fail: Materialize.toast('카카오톡 링크는 모바일 기기에서만 전송 가능합니다.', 4000, 'rounded'),
-              webLink : {
-                text: "[원스토어]인앱SDK 및 검증정책 업데이트 안내!",
-                url: "http://skpla.net/UpgradeCenter"
-              },
+              // webLink : {
+              //   text: "[원스토어]인앱SDK 및 검증정책 업데이트 안내!",
+              //   url: "http://skpla.net/UpgradeCenter"
+              // },
               webButton: {
                 text: "[원스토어]인앱SDK 및 검증정책 업데이트 안내!",
                 url: "http://skpla.net/UpgradeCenter" // 앱 설정의 웹 플랫폼에 등록한 도메인의 URL이어야 합니다.
