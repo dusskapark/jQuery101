dd19116f81df0b1ffa0c8b049f27f7383a7e982f
dusskapark <dusskapark@gmail.com>
2015-10-01 21:46:46 +0900

    버그픽

--- parallax/js/init.js
+++ parallax/js/init.js
@@ -8,7 +8,7 @@
     // 사용할 앱의 JavaScript 키를 설정해 주세요.
     Kakao.init('a230e58b48b10ef26c3ab835c30cb138');
 
-    var shareKakao = function () {
+    shareKakao = function () {
             // 카카오톡 링크 버튼을 생성합니다. 처음 한번만 호출하면 됩니다.
             Kakao.Link.sendTalkLink({
               label: "[OneStore] 인앱SDK 및 검증 정책 업데이트 안내 인앱SDK로 Android 6.0에 완벽 대응하고 업데이트 검증으로 빠르게 출시하세요!",
@@ -29,9 +29,8 @@
             });
             // Kakao.Link.cleanup();
             // 카카오 공유를 GA로 추적
-            ga('send', 'event', "shareLink", "sendkakao", item.id );
-          });
-        }
+            ga('send', 'event', "shareLink", "sendkakao", "UpgradeCenter" );
+          };
 
 
 
